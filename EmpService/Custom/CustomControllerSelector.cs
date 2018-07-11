19953dd94421df0e1d471cf7dd35a5dccb0bf50a
jared lising <jaredlising@gmail.com>
2018-07-11 22:19:48 +0800

    commit to changes of API-AcceptHeader

--- EmpService/Custom/CustomControllerSelector.cs
+++ EmpService/Custom/CustomControllerSelector.cs
@@ -26,21 +26,34 @@ namespace EmpService.Custom
             var controllerName = routeData.Values["controller"].ToString();
 
             string versionNumber = "1";
+
+            //// web API using query string
             //var versionQueryString = HttpUtility.ParseQueryString(request.RequestUri.Query);
             //if(versionQueryString["v"] != null) 
             //{
             //    versionNumber = versionQueryString["v"];
             //}
 
-            string customHeader = "X-StudentService-Version";
-            if(request.Headers.Contains(customHeader))
-            {
-                versionNumber = request.Headers.GetValues(customHeader).FirstOrDefault();
+            //// web API using custom header request
+            //string customHeader = "X-StudentService-Version";
+            //if(request.Headers.Contains(customHeader))
+            //{
+            //    versionNumber = request.Headers.GetValues(customHeader).FirstOrDefault();
 
-                if(versionNumber.Contains(","))
-                {
-                    versionNumber = versionNumber.Substring(0, versionNumber.IndexOf(","));
-                }
+            //    if(versionNumber.Contains(","))
+            //    {
+            //        versionNumber = versionNumber.Substring(0, versionNumber.IndexOf(","));
+            //    }
+            //}
+
+            //// web API using accept header
+            var acceptHeader = request.Headers.Accept
+                                                .Where(ah => ah.Parameters.Count(p => p.Name.ToLower() == "version".ToLower()) > 0);
+
+            // if the var contains any value
+            if(acceptHeader.Any())
+            {
+                versionNumber = acceptHeader.First().Parameters.First(p => p.Name.ToLower() == "version".ToLower()).Value;
             }
 
             if(versionNumber == "1")
