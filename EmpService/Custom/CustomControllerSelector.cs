f9b3ac3a20075917008f404619c9de361b6be634
jared lising <jaredlising@gmail.com>
2018-07-11 18:05:27 +0800

    commiting to API-CustomHeader

--- EmpService/Custom/CustomControllerSelector.cs
+++ EmpService/Custom/CustomControllerSelector.cs
@@ -26,10 +26,21 @@ namespace EmpService.Custom
             var controllerName = routeData.Values["controller"].ToString();
 
             string versionNumber = "1";
-            var versionQueryString = HttpUtility.ParseQueryString(request.RequestUri.Query);
-            if(versionQueryString["v"] != null) 
+            //var versionQueryString = HttpUtility.ParseQueryString(request.RequestUri.Query);
+            //if(versionQueryString["v"] != null) 
+            //{
+            //    versionNumber = versionQueryString["v"];
+            //}
+
+            string customHeader = "X-StudentService-Version";
+            if(request.Headers.Contains(customHeader))
             {
-                versionNumber = versionQueryString["v"];
+                versionNumber = request.Headers.GetValues(customHeader).FirstOrDefault();
+
+                if(versionNumber.Contains(","))
+                {
+                    versionNumber = versionNumber.Substring(0, versionNumber.IndexOf(","));
+                }
             }
 
             if(versionNumber == "1")
